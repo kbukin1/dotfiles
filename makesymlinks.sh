3c01db612e4250cd302069156feb743c6aeafb39
Konstantin Bukin <kbukin@nvidia.com>
2014-09-05 13:25:58 -0700

    added makesymlinks.sh

---
+++ makesymlinks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in `ls $dir`; do
+    if [ $file == `basename $0` ] 
+    then 
+        continue
+    fi
+    if [ -f $olddir/.$file ]
+    then 
+        echo "Skipping .$file"
+        continue
+    fi
+
+    echo "Moving .$file from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to .$file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
